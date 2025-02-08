bdd35de0ff7f04f5dd7f9ab8c076673c33e8dd69
Sechaba Msukwini <sechabamsukwini155@gmail.com>
2025-02-08 08:16:32 +0200

    Seed

--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -1,32 +1,32 @@
 // import bcrypt from 'bcrypt';
 // import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 // const client = await db.connect();
 
 // async function seedUsers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email TEXT NOT NULL UNIQUE,
-//       password TEXT NOT NULL
-//     );
-//   `;
+  // await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await client.sql`
+  //   CREATE TABLE IF NOT EXISTS users (
+  //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  //     name VARCHAR(255) NOT NULL,
+  //     email TEXT NOT NULL UNIQUE,
+  //     password TEXT NOT NULL
+  //   );
+  // `;
 
-//   const insertedUsers = await Promise.all(
-//     users.map(async (user) => {
-//       const hashedPassword = await bcrypt.hash(user.password, 10);
-//       return client.sql`
-//         INSERT INTO users (id, name, email, password)
-//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-//         ON CONFLICT (id) DO NOTHING;
-//       `;
-//     }),
-//   );
+  // const insertedUsers = await Promise.all(
+  //   users.map(async (user) => {
+  //     const hashedPassword = await bcrypt.hash(user.password, 10);
+  //     return client.sql`
+  //       INSERT INTO users (id, name, email, password)
+  //       VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+  //       ON CONFLICT (id) DO NOTHING;
+  //     `;
+  //   }),
+  // );
 
-//   return insertedUsers;
+  // return insertedUsers;
 // }
 
 // async function seedInvoices() {
@@ -102,10 +102,10 @@
 // }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
   // try {
   //   await client.sql`BEGIN`;
   //   await seedUsers();
